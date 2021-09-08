b22cf27516f6f975a410eb317c27eed93755c2fa
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-09-08 07:47:29 +0200

    autentificatication and bcrypt

---
+++ backend/models/Pin.js
@@ -0,0 +1,38 @@
+const { maxHeaderSize } = require('http');
+const { MongoGridFSChunkError } = require('mongodb');
+const mongoose = require ('mongoose');
+
+const PinSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        require:true,
+    },
+    title: {
+        type:String,
+        required:true,
+        min:3
+    },
+    desc: {
+        type:String,
+        require:true,
+        min:3
+    },
+    rating: {
+        type:Number,
+        require:true,
+        min:0,
+        max:5
+    },
+    lat:{
+        type:Number,
+        require:true
+    },
+    long:{
+        type:Number,
+        require:true
+    },
+},
+{timestamps:true}
+);
+
+module.exports = mongoose.model("Pin",PinSchema)
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      HELLOOOOOOO HOPE 
+    </div>
+  );
+}
+
+export default App;
